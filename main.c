33e58f90ee982e46126ec769dda2d4e559d670fa
IRZN <iuryemanuel220@gmail.com>
2025-03-14 04:11:49 -0300

    Adiçao de saida em porcentagem

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main () {
     int pos = 0, neg = 0, z = 0, cont = 0, n;
-
+    int porc_pos, porc_neg, porc_z;
     do {
         printf ("Digite um numero qualquer: \n");
         scanf ("%i", &n);
@@ -22,9 +22,17 @@ int main () {
 
     }while (cont < 20);
 
+    porc_pos = ( pos * 100 ) / cont;
+    porc_neg = ( neg * 100 ) / cont;
+    porc_z = ( z * 100 ) / cont;
+
     printf ("A quantidade de numeros positivos foi: %i \n", pos);
     printf ("A quantidade de numeros negativos foi: %i \n", neg);
     printf ("A quantidade de zeros foi: %i \n", z);
+
+    printf ("A porcentagem de numeros positivos foi: %i \n", porc_pos);
+    printf ("A porcentagem de numeros negativos foi: %i \n", porc_neg);
+    printf ("A porcentagem de zeros foi: %i \n", porc_z);
 
     return 0;
 }
